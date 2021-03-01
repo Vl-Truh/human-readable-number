bb74d2979a048eb59b2ca44377530bf7c3b122d1
Vl-Truh <78600828+Vl-Truh@users.noreply.github.com>
2021-03-01 19:27:59 +0300

    fix: rewrite variables and flow control

--- src/index.js
+++ src/index.js
@@ -1,76 +1,63 @@
-module.exports = function toReadable (n) {
-  let res = '';
-
-    const underTwenty = [
-        'zero',
-        'one',
-        'two',
-        'three',
-        'four',
-        'five',
-        'six',
-        'seven',
-        'eight',
-        'nine',
-        'ten',
-        'eleven',
-        'twelve',
-        'thirteen',
-        'fourteen',
-        'fifteen',
-        'sixteen',
-        'seventeen',
-        'eighteen',
-        'nineteen'
-    ]
-
-    const twoDigit = [
-        'ten',
-        'twenty',
-        'thirty',
-        'forty',
-        'fifty',
-        'sixty',
-        'seventy',
-        'eighty',
-        'ninety'
-    ]
-
-    const threeDigit = [
-        'one hundred',
-        'two hundred',
-        'three hundred',
-        'four hundred',
-        'five hundred',
-        'six hundred',
-        'seven hundred',
-        'eight hundred',
-        'nine hundred',
-
-    ]
-
-    if (n < 20) {
-        res += underTwenty[n];
-    }
-
-    if ((n >= 20 && n <= 90) && (n % 10 == 0)) {
-        res += twoDigit[n / 10 - 1]
-    }
-
-    if ((n >= 20 && n <= 99) && (n % 10 !== 0)) {
-        res += `${twoDigit[Math.floor(n / 10) - 1]} ${underTwenty[n % 10]}`;
-    }
-
-    if (n >= 100 && n % 100 == 0) {
-        res += threeDigit[n / 100 - 1];
-    }
-
-    if ((n > 100) && (n % 10 == 0) && (n % 100 !== 0)) {
-        res += `${threeDigit[Math.floor(n / 100) - 1]} and ${twoDigit[(n % 100) / 10 - 1]}`
-    }
-    if (n > 100 && n % 10 !== 0) {
-        res += `${threeDigit[Math.floor(n / 100) - 1]} and ${twoDigit[Math.floor((n % 100) / 10 - 1)]} ${underTwenty[n % 10]}`
-    }
-
-    return res;
+module.exports = function toReadable (number) {
+	const underTwenty = ["zero",
+                         "one",
+                         "two", 
+                         "three", 
+                         "four", 
+                         "five",
+						 "six",
+                         "seven", 
+                         "eight", 
+                         "nine", 
+                         "ten",
+						 "eleven",
+                         "twelve", 
+                         "thirteen",
+                         "fourteen",
+                         "fifteen",
+						 "sixteen",
+                         "seventeen",
+                         "eighteen",
+                         "nineteen",
+                         "twenty"
+                        ];
+    
+	const tens = ["twenty",
+                  "thirty",
+                  "forty",
+                  "fifty", 
+                  "sixty",
+                  "seventy", 
+                  "eighty", 
+                  "ninety"
+                 ];
+    
+    let final = "";
+    
+	if(number == 0){
+		return underTwenty[0];
+	}
+    
+	if(Math.floor(number/100)){
+		final += underTwenty[Math.floor(number/100)] + " hundred";
+		number -= Math.floor(number/100)*100;
+	}
+    
+	if(Math.floor(number/10)>1 && number != 20){
+		if(final.length != 0){
+			final += " ";
+		}
+		final += tens[Math.floor(number/10)-2];
+		number -= Math.floor(number/10)*10;
+	}
+    
+	if(number == 0){
+			return final;
+	}
+    
+	if(result.length != 0){
+		final += " ";
+	}
+    
+	return final += underTwenty[number]
 }
